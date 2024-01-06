685bd4d60ef341b491f1757bb1ff4f4a316e77ef
ebubekirkarakurt <ebukar75@gmail.com>
2024-01-06 21:03:34 +0300

    .

--- src/components/NavBar/CartButton.tsx
+++ src/components/NavBar/CartButton.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import '../../styles/CartButton.css';
 import '../../styles/SideBarMenu.style.css';
 import CartIcon from './Icons/CartIcon';
-import SideBarMenu from '../sidebar/SideBarMenu';
+import SideBarMenu from '../SideBar/SideBarMenu';
 import { useAppDispatch, useAppSelector } from '../../hooks/hooks';
 import { setVisibility } from '../../redux/reducer/sidebarVisibility';
 import { RootState } from '../../redux/store/store';
